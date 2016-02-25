8b269b8c4b6ed4fa828952ef907fc536a29e10b9
Ben Kuper <bkuperberg@gmail.com>
2016-02-25 21:31:36 +0100

    initial commit

---
+++ Assets/Lib/AMKM/K2M.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityOSC;
+
+public class K2M : MonoBehaviour
+{
+    public GameObject bodyPrefab;
+
+    public string localPort = "9090";
+
+    private Dictionary<int, K2MBody> _Bodies = new Dictionary<int, K2MBody>();
+    OSCServer server;
+
+    void Start()
+    {
+        server = new OSCServer(int.Parse(localPort));
+        server.PacketReceivedEvent += packetReceived;
+    }
+
+    void packetReceived(OSCPacket p)
+    {
+        OSCMessage m = (OSCMessage)p;
+
+        if(m.Address == "/k2m/body/entered")
+        {
+            addBody((int)m.Data[0]);
+        }else if(m.Address == "/k2m/body/left")
+        {
+            removeBody((int)m.Data[0]);
+        }else if(m.Address == "/k2m/body/update")
+        {
+
+        }else if(m.Address == "/k2m/joint")
+        {
+            updatejoint((int)m.Data[0], (int)m.Data[1], new Vector3((float)m.Data[2], (float)m.Data[3], (float)m.Data[4]));
+        }
+    }
+
+    void Update()
+    {
+        server.Update();
+    }
+
+    void addBody(int trackingId)
+    {
+
+        _Bodies[trackingId] = GameObject.Instantiate(bodyPrefab).GetComponent<K2MBody>();
+        _Bodies[trackingId].transform.parent = transform;        
+    }
+
+    void removeBody(int trackingId)
+    {
+        GameObject.Destroy(_Bodies[trackingId].gameObject);
+        _Bodies.Remove(trackingId);
+    }
+
+    void updatejoint(int trackingId, int jointIndex, Vector3 pos)
+    {
+
+        K2MBody b = _Bodies[trackingId];
+        b.updateJoint(jointIndex,pos);
+    }
+}
---
+++ Assets/Lib/AMKM/K2MBody.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class K2MBody : MonoBehaviour {
+
+    public GameObject jointPrefab;
+    public GameObject bonePrefab;
+
+    [Range(.1f, 10f)]
+    public float jointScale = 1;
+
+    [Range(.1f,10f)]
+    public float boneScale = 1;
+
+    public bool boneIsCentered;
+
+    [HideInInspector]
+    public Transform[] joints;
+    [HideInInspector]
+    public Transform[] bones;
+
+    [HideInInspector]
+    public int numJoints = 25;
+    [HideInInspector]
+    public int numBones = 24;
+
+    public enum JointType : int
+    {
+        SpineBase = 0,
+        SpineMid = 1,
+        Neck = 2,
+        Head = 3,
+        ShoulderLeft = 4,
+        ElbowLeft = 5,
+        WristLeft = 6,
+        HandLeft = 7,
+        ShoulderRight = 8,
+        ElbowRight = 9,
+        WristRight = 10,
+        HandRight = 11,
+        HipLeft = 12,
+        KneeLeft = 13,
+        AnkleLeft = 14,
+        FootLeft = 15,
+        HipRight = 16,
+        KneeRight = 17,
+        AnkleRight = 18,
+        FootRight = 19,
+        SpineShoulder = 20,
+        HandTipLeft = 21,
+        ThumbLeft = 22,
+        HandTipRight = 23,
+        ThumbRight = 24,
+    }
+
+    private Dictionary<JointType, JointType> boneMap = new Dictionary<JointType, JointType>()
+    {
+        { JointType.FootLeft, JointType.AnkleLeft },
+        { JointType.AnkleLeft, JointType.KneeLeft },
+        { JointType.KneeLeft, JointType.HipLeft },
+        { JointType.HipLeft, JointType.SpineBase },
+
+        { JointType.FootRight, JointType.AnkleRight },
+        { JointType.AnkleRight, JointType.KneeRight },
+        { JointType.KneeRight, JointType.HipRight },
+        { JointType.HipRight, JointType.SpineBase },
+
+        { JointType.HandTipLeft, JointType.HandLeft },
+        { JointType.ThumbLeft, JointType.HandLeft },
+        { JointType.HandLeft, JointType.WristLeft },
+        { JointType.WristLeft, JointType.ElbowLeft },
+        { JointType.ElbowLeft, JointType.ShoulderLeft },
+        { JointType.ShoulderLeft, JointType.SpineShoulder },
+
+        { JointType.HandTipRight, JointType.HandRight },
+        { JointType.ThumbRight, JointType.HandRight },
+        { JointType.HandRight, JointType.WristRight },
+        { JointType.WristRight, JointType.ElbowRight },
+        { JointType.ElbowRight, JointType.ShoulderRight },
+        { JointType.ShoulderRight, JointType.SpineShoulder },
+
+        { JointType.Head ,JointType.Neck },
+        { JointType.Neck , JointType.SpineShoulder },
+        { JointType.SpineShoulder , JointType.SpineMid },
+        { JointType.SpineMid, JointType.SpineBase }
+       
+
+    };
+
+    // Use this for initialization
+    void Awake () {
+        joints = new Transform[25];
+        bones = new Transform[24];
+
+        for(int i=0;i< numJoints;i++)
+        {
+            joints[i] = GameObject.Instantiate(jointPrefab).transform;
+            joints[i].parent = transform;
+        }
+
+        for(int i=0;i< numBones;i++)
+        {
+            bones[i] = GameObject.Instantiate(bonePrefab).transform;
+            bones[i].parent = transform;
+        }
+	}
+
+    void Start()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        for (int i = 0; i < numJoints; i++)
+        {
+            joints[i].rotation = Quaternion.identity; //force
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void updateJoint(int index,Vector3 pos)
+    {
+        for (int i = 0; i < numJoints; i++)
+        {
+            
+            Transform jt = joints[index];
+            jt.localPosition = pos;
+            jt.localScale = new Vector3(jointScale, jointScale, jointScale);
+        }
+
+
+        for (int i = 0; i < numBones; i++)
+        {
+
+            JointType t1 = (JointType)(i + 1); //start after spineBase
+            JointType t2 = boneMap[t1];
+
+            Transform jt1 = joints[(int)t1];
+            Transform jt2 = joints[(int)t2];
+
+            Transform bone = bones[i];
+
+            if (boneIsCentered)
+            {
+                bone.localPosition = Vector3.Lerp(jt1.localPosition, jt2.localPosition, .5f);
+            }
+            else
+            {
+                bone.localPosition = jt2.localPosition;
+            }
+
+            bone.LookAt(jt1);
+
+            float dist = Vector3.Distance(jt1.position, jt2.position);
+            bone.localScale = new Vector3(boneScale, boneScale, dist* boneScale);
+        }
+    }
+}
